2ca68b78ba6c7c7b8732d955eeaea5f7daab72f7
Magda Dubois <magda.dubois.18@ucl.ac.uk>
2020-06-09 15:30:56 +0200

    new questionnaires

--- models/questionnairesbehaviour.py
+++ models/questionnairesbehaviour.py
@@ -4,24 +4,23 @@ from sqlalchemy import Column, Integer, String, ARRAY, Text
 from models.db import Model
 from models.base_object import BaseObject
 
-
 class QuestionnairesBehaviour(BaseObject, Model):
-    
+
     id = Column(Integer, primary_key=True)
-       
+
     UserNo              = Column(Integer)
-    
+
     Date                = Column(Text(length=10000))
     StartTime           = Column(Text(length=10000))
     FinishTime          = Column(Text(length=10000))
-    
+
     PageNo0             = Column(Text(length=10000))
     PageNo1             = Column(Text(length=10000))
     PageNo2             = Column(Text(length=10000))
     PageNo3             = Column(Text(length=10000))
     PageNo4             = Column(Text(length=10000))
     PageNo5             = Column(Text(length=10000))
-    
+
     IQ_1             = Column(Text(length=10000))
     IQ_2             = Column(Text(length=10000))
     IQ_3             = Column(Text(length=10000))
@@ -30,7 +29,7 @@ class QuestionnairesBehaviour(BaseObject, Model):
     IQ_6             = Column(Text(length=10000))
     IQ_7             = Column(Text(length=10000))
     IQ_8             = Column(Text(length=10000))
-    
+
     IQimage_1             = Column(Text(length=10000))
     IQimage_2             = Column(Text(length=10000))
     IQimage_3             = Column(Text(length=10000))
@@ -39,72 +38,89 @@ class QuestionnairesBehaviour(BaseObject, Model):
     IQimage_6             = Column(Text(length=10000))
     IQimage_7             = Column(Text(length=10000))
     IQimage_8             = Column(Text(length=10000))
-    
-    ASRS                = Column(Text(length=10000))
-    BIS11                = Column(Text(length=10000))
-    IUS                = Column(Text(length=10000))
 
-    
+    ASRS                = Column(Text(length=10000))
+    BIS11               = Column(Text(length=10000))
+    IUS                 = Column(Text(length=10000))
+    LSAS                = Column(Text(length=10000))
+    SDS                 = Column(Text(length=10000))
+    STAI                = Column(Text(length=10000))
+
     def get_id(self):
         return str(self.id)
-    
+
     def get_user_no(self):
         return str(self.UserNo)
-    
+
     def get_date(self):
         return str(self.Date)
-    
+
     def get_start_time(self):
         return str(self.StartTime)
-    
+
     def get_finish_time(self):
         return str(self.FinishTime)
-    
+
     def get_page_no_0(self):
         return str(self.PageNo0)
-    
+
     def get_page_no_1(self):
         return str(self.PageNo1)
-    
+
     def get_page_no_2(self):
         return str(self.PageNo2)
-    
+
     def get_page_no_3(self):
         return str(self.PageNo3)
-    
+
     def get_page_no_4(self):
         return str(self.PageNo4)
-    
+
     def get_page_no_5(self):
         return str(self.PageNo5)
-    
+
     def get_iq_1(self):
         return str(self.IQ_1)
-    
+
     def get_iq_2(self):
         return str(self.IQ_2)
-    
+
     def get_iq_3(self):
         return str(self.IQ_3)
-    
+
     def get_iq_4(self):
         return str(self.IQ_4)
-    
+
     def get_iq_5(self):
         return str(self.IQ_5)
-    
+
     def get_iq_6(self):
         return str(self.IQ_6)
-    
+
     def get_iq_7(self):
         return str(self.IQ_7)
-    
+
     def get_iq_8(self):
         return str(self.IQ_8)
 
     def get_asrs(self):
             return str(self.ASRS)
-    
+
+    def get_bis11(self):
+            return str(self.BIS11)
+
+    def get_ius(self):
+            return str(self.IUS)
+
+    def get_lsas(self):
+            return str(self.LSAS)
+
+    def get_sds(self):
+            return str(self.SDS)
+
+    def get_stai(self):
+            return str(self.STAI)
+
     def errors(self):
         errors = super(QuestionnairesBehaviour, self).errors()
         return errors
--- routes/questionnairesbehaviour.py
+++ routes/questionnairesbehaviour.py
@@ -4,29 +4,26 @@ from models import QuestionnairesBehaviour, BaseObject, db
 from sqlalchemy.sql.expression import func
 
 
-
-
-
 @app.route('/questionnaires_behaviour/<user_id>', methods=['POST', 'GET'])
 
 def create_questionnaires_behaviour(user_id):
-    
-    content                               = request.json        
+
+    content                               = request.json
     questionnairesbehaviour               = QuestionnairesBehaviour()
-    
+
     questionnairesbehaviour.UserNo          = int(user_id)
-    
+
     questionnairesbehaviour.Date            = str(content['Date'])
     questionnairesbehaviour.StartTime       = str(content['StartTime'])
-    questionnairesbehaviour.FinishTime       = str(content['FinishTime'])
-    
+    questionnairesbehaviour.FinishTime      = str(content['FinishTime'])
+
     questionnairesbehaviour.PageNo0         = str(content['PageNo0'])
     questionnairesbehaviour.PageNo1         = str(content['PageNo1'])
-    questionnairesbehaviour.PageNo2         = str(content['PageNo2'])      
-    questionnairesbehaviour.PageNo3         = str(content['PageNo3'])        
-    questionnairesbehaviour.PageNo4         = str(content['PageNo4'])         
+    questionnairesbehaviour.PageNo2         = str(content['PageNo2'])
+    questionnairesbehaviour.PageNo3         = str(content['PageNo3'])
+    questionnairesbehaviour.PageNo4         = str(content['PageNo4'])
     questionnairesbehaviour.PageNo5         = str(content['PageNo5'])
-    
+
     questionnairesbehaviour.IQ_1            = str(content['IQ_1'])
     questionnairesbehaviour.IQ_2            = str(content['IQ_2'])
     questionnairesbehaviour.IQ_3            = str(content['IQ_3'])
@@ -35,7 +32,7 @@ def create_questionnaires_behaviour(user_id):
     questionnairesbehaviour.IQ_6            = str(content['IQ_6'])
     questionnairesbehaviour.IQ_7            = str(content['IQ_7'])
     questionnairesbehaviour.IQ_8            = str(content['IQ_8'])
-    
+
     questionnairesbehaviour.IQimage_1            = str(content['IQimage_1'])
     questionnairesbehaviour.IQimage_2            = str(content['IQimage_2'])
     questionnairesbehaviour.IQimage_3            = str(content['IQimage_3'])
@@ -44,16 +41,16 @@ def create_questionnaires_behaviour(user_id):
     questionnairesbehaviour.IQimage_6            = str(content['IQimage_6'])
     questionnairesbehaviour.IQimage_7            = str(content['IQimage_7'])
     questionnairesbehaviour.IQimage_8            = str(content['IQimage_8'])
-    
-    questionnairesbehaviour.ASRS            = str(content['ASRS'])
+
+    questionnairesbehaviour.ASRS             = str(content['ASRS'])
     questionnairesbehaviour.BIS11            = str(content['BIS11'])
-    questionnairesbehaviour.IUS            = str(content['IUS'])
-
-
-    
+    questionnairesbehaviour.IUS              = str(content['IUS'])
+    questionnairesbehaviour.LSAS             = str(content['LSAS'])
+    questionnairesbehaviour.SDS              = str(content['SDS'])
+    questionnairesbehaviour.STAI             = str(content['STAI'])
 
     BaseObject.check_and_save(questionnairesbehaviour)
 
-    result = dict({"success": "yes"}) 
-    
+    result = dict({"success": "yes"})
+
     return jsonify(result)
